285139ed33eb5eb25b34ce0fc13de4d2a339cc18
santos7ju <santosjulialuiza@gmail.com>
2025-12-03 23:01:15 -0300

    Front gestor: cadastro e visualizaç F    oã de projetos e emendas; designação e alteração do relator; exportação de excel com informações de relatores e seus projetos

--- core/models.py
+++ core/models.py
@@ -1,7 +1,15 @@
 # core/models.py
 
 from django.db import models
-from django.contrib.auth.models import User # Vamos usar o usuário padrão
+from django.contrib.auth.models import User
+
+class PlataformaBrasilService:
+    @staticmethod
+    def receber_credenciais(email, senha):
+        print(f"--- [MOCK] PLATAFORMA BRASIL ---")
+        print(f"E-mail: {email}")
+        print(f"Senha: {senha}")
+        return True
 
 class Pesquisador(models.Model):
     """
@@ -34,6 +42,14 @@ class Projeto(models.Model):
     caae = models.CharField("CAAE", max_length=254, unique=True, help_text="Certificado de Apresentação para Apreciação Ética")
     protocolo = models.CharField("Protocolo", max_length=254, null=True, blank=True)
     
+    arquivo_pdf = models.FileField(
+        "Arquivo do Projeto (PDF)", 
+        upload_to='projetos/pdfs/', 
+        null=True, 
+        blank=True
+    )
+
+    
     pesquisador = models.ForeignKey(
         Pesquisador, 
         on_delete=models.PROTECT, # Não permite deletar o pesquisador se ele tiver projetos
@@ -61,6 +77,28 @@ class Projeto(models.Model):
     def __str__(self):
         return f"{self.titulo} ({self.caae})"
 
+class Emenda(models.Model):
+    """
+    Uma emenda é uma alteração ou adição a um projeto existente.
+    """
+    projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE, related_name='emendas')
+    titulo = models.CharField("Título da Emenda", max_length=255)
+    descricao = models.TextField("Descrição das Alterações")
+    data_submissao = models.DateTimeField(auto_now_add=True)
+    
+    # Campo de PDF para a Emenda
+    arquivo_pdf = models.FileField(
+        "Arquivo da Emenda (PDF)", 
+        upload_to='emendas/pdfs/', 
+        null=True, 
+        blank=True
+    )
+
+    aprovada = models.BooleanField("Emenda Aprovada?", default=False)
+
+    def __str__(self):
+        return f"Emenda: {self.titulo} -> {self.projeto.titulo}"
+
 
 class Parecer(models.Model):
     DECISAO_CHOICES = (
@@ -77,3 +115,5 @@ class Parecer(models.Model):
 
     def __str__(self):
         return f"Parecer de {self.relator.username} para {self.projeto.titulo}"
+        
+    
